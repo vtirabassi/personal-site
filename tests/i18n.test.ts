a7977fa268c1971ee504f15e51335595c6b531ba
Vinicius Tirabassi de Oliveira <vinicius@Viniciuss-Laptop.local>
2026-05-30 14:55:30 -0300

    fix(i18n): remove redundant as const and add nested key coverage to tests

--- tests/i18n.test.ts
+++ tests/i18n.test.ts
@@ -18,4 +18,24 @@ describe('ui dictionary', () => {
     expect(ui.pt.monthNames).toHaveLength(12);
     expect(ui.en.monthNames).toHaveLength(12);
   });
+
+  it('pt.home and en.home have the same keys', () => {
+    expect(Object.keys(ui.pt.home).sort()).toEqual(Object.keys(ui.en.home).sort());
+  });
+
+  it('pt.blog and en.blog have the same keys', () => {
+    expect(Object.keys(ui.pt.blog).sort()).toEqual(Object.keys(ui.en.blog).sort());
+  });
+
+  it('pt.tracks and en.tracks have the same keys', () => {
+    expect(Object.keys(ui.pt.tracks).sort()).toEqual(Object.keys(ui.en.tracks).sort());
+  });
+
+  it('pt.library and en.library have the same keys', () => {
+    expect(Object.keys(ui.pt.library).sort()).toEqual(Object.keys(ui.en.library).sort());
+  });
+
+  it('pt.post and en.post have the same keys', () => {
+    expect(Object.keys(ui.pt.post).sort()).toEqual(Object.keys(ui.en.post).sort());
+  });
 });
